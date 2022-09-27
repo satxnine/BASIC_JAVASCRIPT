9e367029fc6c4a94f0b2128c1fb9b42f9bb695b6
satxnine <fnuraini503@gmail.com>
2022-09-27 13:23:59 +0700

    basic

---
+++ mathOperator_4_incrementDecrement.js
@@ -0,0 +1,24 @@
+//-----Increment-----
+var a = 0
+var b = 10
+var a = b++
+console.log(a)
+console.log(b)
+
+var c = 0
+var d = 10
+var c = ++d
+console.log(c)
+console.log(d)
+
+var e = 0
+var f = 10
+var e = f--
+console.log(e)
+console.log(f)
+
+var g = 0
+var h = 10
+var g = --h
+console.log(g)
+console.log(h)
---
+++ mathOperator_4_latihan.js
@@ -0,0 +1,49 @@
+console.log('-----Increment-----')
+var a = 0
+var b = 10
+var a = b++
+console.log(a)
+
+var c = 0;
+var d = 10
+d++;
+d++;
+console.log(d);
+
+var e = 0;
+var f = 10
+++f;
+++f;
+console.log(f);
+
+var g = 0;
+var h = 10
+++h;
+++h;
+console.log(h);
+
+console.log('-----Decrement-----')
+var i = 0;
+var j = 10;
+console.log(j);
+
+
+var k = 0;
+var l = 10;
+l--;
+l--;
+console.log(l);
+
+
+var m = 0;
+var n = 10;
+--n;
+--n;
+console.log(n);
+
+
+var o = 0;
+var p = 10;
+--p;
+--p;
+console.log(p);
