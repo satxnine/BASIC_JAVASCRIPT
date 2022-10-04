9a2b3eecd213b1eda98b887859c1362cffaacac7
satxnine <fnuraini503@gmail.com>
2022-10-04 14:16:42 +0700

    Conditionals and Loops

---
+++ ConditionalsAndLoops_else_if_Statement_1.js
@@ -0,0 +1,9 @@
+
+var course = 1;
+if (course == 1) {
+    console.log("HTML Tutorial");
+} else if (course == 2) {
+    console.log("CSS Tutorial");
+} else {
+    console.log("JavaScript Tutorial");
+}
